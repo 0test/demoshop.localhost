b77d1b51b0cd1e7d7f3b2640e7598ad9606ff1c1
Andrey <0test@mail.ru>
2025-05-23 17:11:48 +0300

    fix empty options
    Fix empty options in email's templates

---
+++ views/cart/sendpage/reports/optionsTpl.blade.php
@@ -0,0 +1,3 @@
+<div class="badge " data-key="{{$data['key']}}">
+	{{ $data['option'] }}
+</div>
--- views/cart/sendpage/reports/order_report_admin.blade.php
+++ views/cart/sendpage/reports/order_report_admin.blade.php
@@ -3,8 +3,13 @@
 
 <h4>Данные покупателя:</h4>
 
+
+<ul>
+    <li>Имя: {{ $data['order']['name'] }}</li>
+    <li>Почта: {{ $data['order']['email'] }}</li>
+    <li>Телефон: {{ $data['order']['phone'] }}</li>
+</ul>
 <p>
-	{{ $data['order']['name'] }}, {{ $data['order']['email'] }}, {{ $data['order']['phone'] }} <br>
 	Способ доставки: {{ $data['order']['fields']['delivery_method_title'] ?? ''}}<br>
 	Способ оплаты: {{ $data['order']['fields']['payment_method_title'] ?? ''}}
 </p>
@@ -18,5 +23,6 @@
 	'ownerTPL'=>'@B_FILE:cart.sendpage.reports..order_report_items',
 	'subtotalsTpl' => '@B_FILE:cart.sendpage.reports.order_report_subtotals',
 	'subtotalsRowTpl'=>'@B_FILE:cart.sendpage.reports.order_report_subtotals_row',
+    'optionsTpl' => '@B_FILE:cart.sendpage.reports.optionsTpl',
 	'urlScheme'=>'full'
 ]) !!}
